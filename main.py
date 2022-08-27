698836e48c33b7b4a80e7a7de6a37b5be10b7add
Kragline <armanaraqelyan1107@gmail.com>
2022-08-27 16:36:16 +0400

    some update

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ while True:
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     faces = faces_db.detectMultiScale(gray_img, 1.1, 19)
-
+    cv2.putText(img, 'Press Q to close', (10, 20), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 0, 0), 2)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
