2fd22737e9c97eb4b977b5ef145a6e60b1a46ca8
Kragline <armanaraqelyan1107@gmail.com>
2022-09-01 18:35:06 +0400

    Face detecting

--- Tracking.py
+++ Tracking.py
@@ -1,3 +1,4 @@
+from cgitb import reset
 import cv2
 import mediapipe as mp
 
@@ -5,10 +6,15 @@ import mediapipe as mp
 class Tracking():
     def __init__(self):
         self.mp_draw = mp.solutions.drawing_utils
+
         self.mp_pose = mp.solutions.pose
         self.pose = self.mp_pose.Pose()
+
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands()
+
+        self.mp_face = mp.solutions.face_detection
+        self.face = self.mp_face.FaceDetection()
 
     def get_pose_landmarks(self, img, draw=False):
         pose_landmarks_list = []
@@ -40,3 +46,20 @@ class Tracking():
                         self.mp_draw.draw_landmarks(img, one_hand, self.mp_hands.HAND_CONNECTIONS)
 
         return hands_landmarks_list
+
+    def get_face_landmarks(self, img, draw=False):
+        face_landmarks_list = []
+        rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.face.process(rgb_img)
+
+        if results.detections:
+            for num, detection in enumerate(results.detections):
+                height, width = img.shape[:2]
+                det_bbox = detection.location_data.relative_bounding_box
+                bbox = int(det_bbox.xmin * width), int(det_bbox.ymin * height), int(det_bbox.width * width), int(det_bbox.height * height)
+                face_landmarks_list.append([bbox, int(detection.score[0])])
+                
+                if draw == True:
+                    self.mp_draw.draw_detection(img, detection)
+
+        return face_landmarks_list
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
 
-    print(tr.get_pose_landmarks(img, True))
-    print(tr.get_hand_landmarks(img, True))
+    # print(tr.get_pose_landmarks(img, True))
+    # print(tr.get_hand_landmarks(img, True))
+    print(tr.get_face_landmarks(img, True))
 
     cv2.imshow('Result', img)
     if cv2.waitKey(1) & 0xff == ord('q'):
